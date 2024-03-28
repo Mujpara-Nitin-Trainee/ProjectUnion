8dfed9c28be60efda9a57974c0f40878bbf6d6a1
Mujpara-Nitin-Trainee <nitin.mujpara.2024@gmail.com>
2024-03-28 16:18:14 +0530

    Delimiter Search Done

--- controllers/paggingController.js
+++ controllers/paggingController.js
@@ -8,7 +8,8 @@ const {
   AttendanceReportService,
   pageSearchService,
   pageSeachOptimiumService,
-  pageSeachOptimiumLimitService
+  pageSeachOptimiumLimitService,
+  pageDelimiterSearchService
 } = require("../services/paggingService");
 
 const pagging = async (req, res) => {
@@ -269,6 +270,97 @@ const pageSeachOptimium = async(req,res) => {
     }
 }
 
+const pageDelimiterSearch = async(req,res) => {
+    try{
+
+        const data = req.body.search;
+
+        let values = [];
+        let i = 0;
+        for(let j=1; j <= data.length; j++){
+            
+            if((data.charAt(i) == '_' || data.charAt(i) == '^' || data.charAt(i) == '$' || 
+                data.charAt(i) == '}' || data.charAt(i) == '{' || data.charAt(i) == '#') && 
+                (data.charAt(j) == '_' || data.charAt(j) == '^' || data.charAt(j) == '$' || 
+                data.charAt(j) == '}' || data.charAt(j) == '{' || data.charAt(j) == '#')){
+
+                let answer = data.substring(i,j);
+                values.push(answer);
+                i = j;
+            
+            }else if((data.charAt(i) == '_' || data.charAt(i) == '^' || data.charAt(i) == '$' || 
+            data.charAt(i) == '}' || data.charAt(i) == '{' || data.charAt(i) == '#') && j == data.length){
+                let answer = data.substring(i,j);
+                values.push(answer);
+                i = j;
+            }
+        }
+
+        let firstname = [];
+        let lastname = [];
+        let email = [];
+        let age = [];
+        let gender = [];
+        let mobileno = [];
+
+        for(let k=0; k < values.length ; k++){
+
+            if(values[k].charAt(0) == '_'){
+                firstname.push(`name LIKE '%${values[k].slice(1)}%'`); 
+            }else if(values[k].charAt(0) == '^'){
+                lastname.push(`surname LIKE '%${values[k].slice(1)}%'`);
+            }else if(values[k].charAt(0) == '$'){
+                email.push(`email LIKE '%${values[k].slice(1)}%'`);
+            }else if(values[k].charAt(0) == '}'){
+                age.push(values[k].slice(1));
+            }else if(values[k].charAt(0) == '{'){
+                gender.push(values[k].slice(1));
+            }else if(values[k].charAt(0) == '#'){
+                mobileno.push(`mobile_no LIKE '%${values[k].slice(1)}%'`);
+            }
+        }
+
+        if(firstname.length > 0) { firstname.join("or") }
+        if(lastname.length > 0) { lastname.join("or") }
+        if(email.length > 0) { email.join("or") }
+        if(age.length > 0) { age.join("or") }
+        if(gender.length > 0) { gender.join("or") }
+        if(mobileno.length > 0) { mobileno.join("or") }
+
+        function isEmpty(val) {
+            return (val === null || val === "" || val === "undefined" || val.length <= 0 || val === 0) ? true : false;
+        }
+        if(isEmpty(firstname)){
+            firstname.push(`firstname LIKE '%'`);
+        }
+        if(isEmpty(lastname)){
+            lastname.push(`surname LIKE '%'`);
+        }
+        if(isEmpty(mobileno)){
+            mobileno.push(`mobile_no LIKE '%'`);
+        }
+        if(isEmpty(email)){
+            email.push(`email LIKE '%'`)
+        }
+
+        console.log(firstname + " " + lastname + " " + email + " " + mobileno);
+
+        const [response] = await pageDelimiterSearchService(firstname,lastname,email,mobileno);
+
+        console.log(response);
+
+        if(response.length > 0){
+            res.render('delimiterSearch',{student:response,rows:Object.keys(response[0])});
+        }else{
+            res.render('delimiterSearch',{student:response, rows:0});
+        }
+
+
+    }catch(err){
+        console.log(err);
+    }
+}
+
 module.exports = {
   pagging,
   pageOrderby,
@@ -278,5 +370,6 @@ module.exports = {
   singleAttendance,
   pageSearching,
   pageSearchFilter,
-  pageSeachOptimium
+  pageSeachOptimium,
+  pageDelimiterSearch
 };
--- routers/pagginationRouter.js
+++ routers/pagginationRouter.js
@@ -11,7 +11,8 @@ const {
   singleAttendance,
   pageSearching,
   pageSearchFilter,
-  pageSeachOptimium
+  pageSeachOptimium,
+  pageDelimiterSearch
 } = require("../controllers/paggingController");
 
 router.get("/", auth, pagging);
@@ -28,5 +29,11 @@ router.get("/searching", auth, pageSearching);
 router.post("/searching/filter",auth, pageSearchFilter);
 router.get("/searching/optimalFilter",auth,pageSeachOptimium);
 
+router.get("/searching/delimiterSearch",auth, (req,res) => {
+    let rows;
+    res.render('delimiterSearch',{rows});
+})
+
+router.post("/searching/delimiterSearch", auth, pageDelimiterSearch)
 
 module.exports = router;
