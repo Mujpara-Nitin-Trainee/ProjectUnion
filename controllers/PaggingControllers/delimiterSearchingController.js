6aa0b451df7775e47ccfc1d9b571ee0002dbedfa
Mujpara-Nitin-Trainee <nitin.mujpara.2024@gmail.com>
2024-07-27 18:53:30 +0530

    Fix: solved bugs in delimeter search

--- controllers/PaggingControllers/delimiterSearchingController.js
+++ controllers/PaggingControllers/delimiterSearchingController.js
@@ -1,26 +1,26 @@
-const {pageDelimiterSearchService} = require("../../services/PaggingServices/delimiterSearchService");
+const { pageDelimiterSearchService } = require("../../services/PaggingServices/delimiterSearchService");
 
-const pageDelimiterSearch = async(req,res) => {
-    try{
+const pageDelimiterSearch = async (req, res) => {
+    try {
 
         const data = req.body.search;
 
         let values = [];
         let i = 0;
-        for(let j=1; j <= data.length; j++){
-            
-            if((data.charAt(i) == '_' || data.charAt(i) == '^' || data.charAt(i) == '$' || 
-                data.charAt(i) == '}' || data.charAt(i) == '{' || data.charAt(i) == '#') && 
-                (data.charAt(j) == '_' || data.charAt(j) == '^' || data.charAt(j) == '$' || 
-                data.charAt(j) == '}' || data.charAt(j) == '{' || data.charAt(j) == '#')){
+        for (let j = 1; j <= data.length; j++) {
 
-                let answer = data.substring(i,j);
+            if ((data.charAt(i) == '_' || data.charAt(i) == '^' || data.charAt(i) == '$' ||
+                data.charAt(i) == '}' || data.charAt(i) == '{' || data.charAt(i) == '#') &&
+                (data.charAt(j) == '_' || data.charAt(j) == '^' || data.charAt(j) == '$' ||
+                    data.charAt(j) == '}' || data.charAt(j) == '{' || data.charAt(j) == '#')) {
+
+                let answer = data.substring(i, j);
                 values.push(answer);
                 i = j;
-            
-            }else if((data.charAt(i) == '_' || data.charAt(i) == '^' || data.charAt(i) == '$' || 
-            data.charAt(i) == '}' || data.charAt(i) == '{' || data.charAt(i) == '#') && j == data.length){
-                let answer = data.substring(i,j);
+
+            } else if ((data.charAt(i) == '_' || data.charAt(i) == '^' || data.charAt(i) == '$' ||
+                data.charAt(i) == '}' || data.charAt(i) == '{' || data.charAt(i) == '#') && j == data.length) {
+                let answer = data.substring(i, j);
                 values.push(answer);
                 i = j;
             }
@@ -33,62 +33,64 @@ const pageDelimiterSearch = async(req,res) => {
         let gender = [];
         let mobileno = [];
 
-        for(let k=0; k < values.length ; k++){
+        for (let k = 0; k < values.length; k++) {
 
-            if(values[k].charAt(0) == '_'){
-                firstname.push(`name LIKE '%${values[k].slice(1)}%'`); 
-            }else if(values[k].charAt(0) == '^'){
+            if (values[k].charAt(0) == '_') {
+                firstname.push(`name LIKE '%${values[k].slice(1)}%'`);
+            } else if (values[k].charAt(0) == '^') {
                 lastname.push(`surname LIKE '%${values[k].slice(1)}%'`);
-            }else if(values[k].charAt(0) == '$'){
+            } else if (values[k].charAt(0) == '$') {
                 email.push(`email LIKE '%${values[k].slice(1)}%'`);
-            }else if(values[k].charAt(0) == '}'){
+            } else if (values[k].charAt(0) == '}') {
                 age.push(values[k].slice(1));
-            }else if(values[k].charAt(0) == '{'){
+            } else if (values[k].charAt(0) == '{') {
                 gender.push(values[k].slice(1));
-            }else if(values[k].charAt(0) == '#'){
+            } else if (values[k].charAt(0) == '#') {
                 mobileno.push(`mobile_no LIKE '%${values[k].slice(1)}%'`);
             }
-        }
 
-        if(firstname.length > 0) { firstname.join("or") }
-        if(lastname.length > 0) { lastname.join("or") }
-        if(email.length > 0) { email.join("or") }
-        if(age.length > 0) { age.join("or") }
-        if(gender.length > 0) { gender.join("or") }
-        if(mobileno.length > 0) { mobileno.join("or") }
+        }
 
         function isEmpty(val) {
             return (val === null || val === "" || val === "undefined" || val.length <= 0 || val === 0) ? true : false;
         }
-        if(isEmpty(firstname)){
-            firstname.push(`firstname LIKE '%'`);
+        if (isEmpty(firstname)) {
+            firstname.push(`name LIKE '%'`);
         }
-        if(isEmpty(lastname)){
+        if (isEmpty(lastname)) {
             lastname.push(`surname LIKE '%'`);
         }
-        if(isEmpty(mobileno)){
+        if (isEmpty(mobileno)) {
             mobileno.push(`mobile_no LIKE '%'`);
         }
-        if(isEmpty(email)){
+        if (isEmpty(email)) {
             email.push(`email LIKE '%'`)
         }
 
+        if (firstname.length > 0) { firstname = firstname.join(' or ') }
+        if (lastname.length > 0) { lastname = lastname.join(' or ') }
+        if (email.length > 0) { email = email.join("or") }
+        if (age.length > 0) { age = age.join("or") }
+        if (gender.length > 0) { gender = gender.join("or") }
+        if (mobileno.length > 0) { mobileno = mobileno.join("or") }
+
+
         // console.log(firstname + " " + lastname + " " + email + " " + mobileno);
 
-        const [response] = await pageDelimiterSearchService(firstname,lastname,email,mobileno);
+        const [response] = await pageDelimiterSearchService(firstname, lastname, email, mobileno);
 
         // console.log(response);
 
-        if(response.length > 0){
-            res.render('delimiterSearch',{student:response,rows:Object.keys(response[0])});
-        }else{
-            res.render('delimiterSearch',{student:response, rows:0});
+        if (response.length > 0) {
+            res.render('delimiterSearch', { student: response, rows: Object.keys(response[0]) });
+        } else {
+            res.render('delimiterSearch', { student: response, rows: 0 });
         }
 
 
-    }catch(err){
+    } catch (err) {
         console.log(err);
     }
 }
 
-module.exports = {pageDelimiterSearch};
+module.exports = { pageDelimiterSearch };
--- controllers/PaggingControllers/studentResultGridController.js
+++ controllers/PaggingControllers/studentResultGridController.js
@@ -1,74 +1,76 @@
-const {resultGridService,singleResultService,singleAttendanceService} = require("../../services/PaggingServices/studentResultService");
+const { resultGridService, singleResultService, singleAttendanceService } = require("../../services/PaggingServices/studentResultService");
 
 const resultGrid = async (req, res) => {
-    try {
-      const page = req.query.page || 1;
-      let offset = (page - 1) * 20;
-  
-      const [response] = await resultGridService(offset);
-      // console.log(response);
-  
-      res.render("resultGrid", { student: response, page });
-    } catch (err) {
-      console.log(err);
-    }
-};
-  
-const singleResult = async (req, res) => {
-    try {
-      const sid = req.query.id || 1;
-      let total_mark = 0;
-  
-      const [response] = await singleResultService(sid);
-  
-      for (let i = 0; i < response.length; i++) {
-        total_mark = total_mark + response[i].total_marks;
-      }
-      // console.log(total_marks);
-  
-      const [attendance] = await singleAttendanceService(sid);
-      // console.log(attendance[0].Present_days);
-  
-      res.render("singleResultGrid", {
-        student: response,
-        total_mark,
-        attendance: attendance,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-};
-  
-const singleAttendance = async (req, res) => {
-    try {
-      const id = req.query.id || 1;
-      const date = req.query.month || 12;
-      // console.log(id);
-      // console.log(date);
-  
-      let month = 12;
-      let year = 2023;
-  
-      if (date == 12) {
-        month = 12;
-        year = 2023;
-      }
-      if (date == 1) {
-        month = 1;
-        year = 2024;
-      }
-      if (date == 2) {
-        month = 2;
-        year = 2024;
-      }
-  
-      const [response] = await AttendanceReportService(month, year, id);
-      // console.log(response);
-  
-      res.render("attendanceReport", { student: response[0], month, id });
-    } catch (err) {
-      console.log(err);
-    }
+  try {
+    const page = req.query.page || 1;
+    let offset = (page - 1) * 20;
+
+    const [response] = await resultGridService(offset);
+    // console.log(response);
+
+    res.render("resultGrid", { student: response, page });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-module.exports = {resultGrid,singleResult,singleAttendance};
+const singleResult = async (req, res) => {
+  try {
+    const sid = req.query.id || 1;
+    let total_mark = 0;
+
+    const [response] = await singleResultService(sid);
+
+    console.log(response);
+
+    for (let i = 0; i < response.length; i++) {
+      total_mark = total_mark + response[i].total_marks;
+    }
+    // console.log(total_marks);
+
+    const [attendance] = await singleAttendanceService(sid);
+    // console.log(attendance[0].Present_days);
+
+    res.render("singleResultGrid", {
+      student: response,
+      total_mark,
+      attendance: attendance,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const singleAttendance = async (req, res) => {
+  try {
+    const id = req.query.id || 1;
+    const date = req.query.month || 12;
+    // console.log(id);
+    // console.log(date);
+
+    let month = 12;
+    let year = 2023;
+
+    if (date == 12) {
+      month = 12;
+      year = 2023;
+    }
+    if (date == 1) {
+      month = 1;
+      year = 2024;
+    }
+    if (date == 2) {
+      month = 2;
+      year = 2024;
+    }
+
+    const [response] = await AttendanceReportService(month, year, id);
+    // console.log(response);
+
+    res.render("attendanceReport", { student: response[0], month, id });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = { resultGrid, singleResult, singleAttendance };
